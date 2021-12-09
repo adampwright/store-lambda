441d31f42df6c6d196049fe995ed3616aff87453
adampwright <adam@wrightwaydigitalmedia.com>
2021-12-08 19:28:26 -0800

    working change order status functiuon

--- customer-arrived-pickup/index.js
+++ customer-arrived-pickup/index.js
@@ -1,14 +1,12 @@
 require('dotenv').config();
 const axios = require('axios');
 
-const changeOrderStatus = async (entityId) => {
+const changeOrderStatus = async (requestBody) => {
   const config = {
     method: 'post',
     url: `https://api.wrightwaystore.com/index.php/rest/V1/orders/`,
     headers: { Authorization: `Bearer ${process.env.BACKEND_JWT}` },
-    data: {
-        entity = { entity_id: entityId, state: 'processing', status: 'customer_arrived' }
-    },
+    data: requestBody,
   };
 
   try {
@@ -28,6 +26,6 @@ const sendRes = (status, body, filename) => {
 };
 
 exports.handler = async (event, context, callback) => {
-  const orders = await changeOrderStatus(event.entity_id);
+  const orders = await changeOrderStatus(event);
   return sendRes(200, orders);
 };
--- customer-arrived-pickup/test.js
+++ customer-arrived-pickup/test.js
@@ -1,26 +1,27 @@
 require('dotenv').config();
 const axios = require('axios');
 
-const changeOrderStatus = async () => {
-  const config = {
-    method: 'post',
-    url: `https://api.wrightwaystore.com/index.php/rest/V1/orders/`,
-    headers: { Authorization: `Bearer ${process.env.BACKEND_JWT}` },
-    data: {
-      entity: entity,
-    },
+const changeOrderStatus = async (requestBody) => {
+  const reqBody = JSON.parse(requestBody)
+
+    const config = {
+      method: 'post',
+      url: `https://api.wrightwaystore.com/index.php/rest/V1/orders/`,
+      headers: { Authorization: `Bearer ${process.env.BACKEND_JWT}` },
+      data: {
+          entity = reqBody
+      },
+    };
+
+    try {
+      let res = await axios(config);
+      return res.data;
+    } catch (err) {
+      console.log(err);
+    }
   };
 
-  try {
-    let res = await axios(config);
-    return res.data;
-  } catch (err) {
-    console.log(err);
-  }
-};
-
 const startFunct = async () => {
-  const entity = { entity_id: 21, state: 'processing', status: 'customer_arrived' };
   const changeStatus = await changeOrderStatus();
   console.log(changeStatus);
 };
